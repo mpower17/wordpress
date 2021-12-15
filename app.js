cc34b2611ebf4db37899fc31afcc0ca0fe0fccc4
mpower17 <maxim.zaykov97@gmail.com>
2021-12-15 20:49:33 +0300

    добавляет базовый сервис

---
+++ app.js
@@ -0,0 +1,61 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Accept, Authorization'
+};
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function login(req, res) {
+    res.set(CORS);
+    res.send("itmo286434");
+}
+
+function render(req, res) {
+    const tempPath1 = path.resolve();
+    res.sendFile(path.join(tempPath1, "index.html"));
+}
+
+async function wordpress(req, res) {
+    const content = req.query.content;     
+        
+    const getToken = await fetch("https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: "gossjsstudent2017", password: "|||123|||456" }),
+    });
+
+    const { token } = await getToken.json();
+        
+        
+    const createPost = await fetch("https://wordpress.kodaktor.ru/wp-json/wp/v2/posts", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            'status': 'publish',
+            'title': login,
+            'content': content
+        })
+    })
+}
+
+app.all('/login/', login)
+app.all('/', render)
+app.get('/wordpress/', wordpress)
+app.all('*', login)
+
+
+app.listen(process.env.PORT);
