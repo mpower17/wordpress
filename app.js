f43edbd8a68e20cd7a3e7be6457256149713fb5b
mpower17 <maxim.zaykov97@gmail.com>
2021-12-15 21:26:59 +0300

    исправляет ошибку

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
-import fetch from 'node-fetch';
+import axios from "axios";
 
 const CORS = {
     'Access-Control-Allow-Origin': '*',
@@ -23,47 +23,24 @@ function render(req, res) {
     res.sendFile(path.join(tempPath1, "index.html"));
 }
 
-async function wordpress(req, res) {
-    const content = req.query.content;     
-    
-    console.error(content);
-
-    try {
-        const getToken = await fetch("https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username: "gossjsstudent2017", password: "|||123|||456" }),
-        });
-
-    
-        const { token } = await getToken.json();
-
-        console.error(token);
-
-        const createPost = await fetch("https://wordpress.kodaktor.ru/wp-json/wp/v2/posts", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-                status: "publish",
-                title: "mpower17",
-                content: content
-            })
-        })
-    } catch (e) {
-        console.error(e);
-    }
-}
-
 app.all('/login/', login)
 app.all('/', render)
-app.get('/wordpress/', wordpress)
+
+app.get('/wordpress/', async (req, res, next) => {
+    const content = req.query.content;
+    const response = await axios.post('https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token', { username: 'gossjsstudent2017', password: '|||123|||456' });
+    const token = response.data.token;
+    
+    const wordpressResponse = await axios.post('https://wordpress.kodaktor.ru/wp-json/wp/v2/posts', { content, title: 'mpower17', status: 'publish' },
+    {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    
+    res.send(wordpressResponse.data.id + '');
+});
+
 app.all('*', login)
 
 
